680b59b29e66adece2bfee657c697662e8aeff37
Rodrigo <gil00013@algonquinlive.com>
2021-04-13 11:07:08 -0400

    Added XSS/Mongo Sanitize and created registration and jwt token routes

--- models/User.js
+++ models/User.js
@@ -1,13 +1,45 @@
-
 import mongoose from 'mongoose'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+
+const saltRounds = 14
+const jwtSecretKey = 'superSecretKey'
 
 const schema = new mongoose.Schema({
-    firstName: { type: String, required: true, maxLength: 64 },
-    lastName: { type: String, required: true, maxLength: 64 },
-    email: { type: String, required: true, maxLength: 512, Unique: true },
-    password: { type: String, required: true, maxLength: 70 }
-    }
-)
+  firstName: { type: String, trim: true, maxLength: 64, required: true },
+  lastName: { type: String, trim: true, maxLength: 64, required: true },
+  email: {
+    type: String,
+    trim: true,
+    maxLength: 512,
+    unique: true,
+    required: true,
+  },
+  password: { type: String, trim: true, maxLength: 70, required: true },
+})
+
+schema.methods.generateAuthToken = function () {
+  const payload = { uid: this._id }
+  return jwt.sign(payload, jwtSecretKey)
+}
+
+schema.statics.authenticate = async function (email, password) {
+  const user = await this.findOne({ email: email })
+
+  const badHash = `$2b$${saltRounds}$invalidusernameaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa`
+  const hashedPassword = user ? user.password : badHash
+  const passwordDidMatch = await bcrypt.compare(password, hashedPassword)
+
+  return passwordDidMatch ? user : null
+}
+
+schema.pre('save', async function (next) {
+  // Only encrypt if the password property is being changed.
+  if (!this.isModified('password')) return next()
+
+  this.password = await bcrypt.hash(this.password, saltRounds)
+  next()
+})
 
 const Model = mongoose.model('User', schema)
 
